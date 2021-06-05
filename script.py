ff5eb91a3bb375299b3fe8327264540241ce1e0b
Ranjith KR <ranjith@whiterabbit.group>
2021-06-05 22:00:53 +0530

    First Commit

---
+++ script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import requests
+import socket
+import ssl
+
+
+#curl -s -o /dev/null -I -w "%{http_code}" http://deployer.wrg-apps.com
+#curl -I "http://deployer.wrg-apps.com"
+
+sites = open("demofile.txt", "r")
+for dev_urls in sites:
+    #print(x.rstrip())
+    response = requests.get(url=dev_urls.rstrip(), allow_redirects=True)
+    #print(response)
+    print(response.url, response.status_code)
